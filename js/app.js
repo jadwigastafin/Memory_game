33dcc9166daa5158fc8ef06deed014720628de13
jadwigastafin <jadwiga.stafin@gmail.com>
2017-11-15 11:20:36 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const cards = [];
+
+const card = document.getElementsById('s0');
+
+c0.addEventListener("click", function(){
+    revealCard(0);
+});
+
+function revealCard(nr){
+    alert(nr);
+}
