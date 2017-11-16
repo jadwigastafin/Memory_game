a1bd8de5b48064f933df334bd2686b3e8a9ce72b
jadwigastafin <jadwiga.stafin@gmail.com>
2017-11-16 10:36:36 +0100

    board

--- js/out.js
+++ js/out.js
@@ -70,17 +70,121 @@
 "use strict";
 
 
-var cards = [];
+$(function () {
+    //Funkcja samowywołująca zabezpiecza skrypt, treśc nie dostępna na zewnatrz
+    console.log('DOM');
 
-var card = document.getElementsById('s0');
+    var tilesCount = 20; //ilość kafelków na planszy
+    var tiles = []; //tablica z wygenerowanymi numerkami kafelków
+    var clickedTiles = []; //kliknięte kafelki max2
+    var canGet = true; //zabezpieczenie przed kliknięciem wiecej niż 2 kafelków
+    var movesCount = 0; //liczba ruchów gracza
+    var tilesPair = 0; //sparowane kafelki max = tilesCount/2
 
-c0.addEventListener("click", function () {
-    revealCard(0);
+
+    startGame = function startGame() {
+        tiles = [];
+        clickedTiles = [];
+        canGet = true;
+        movesCount = 0;
+        tilesPair = 0;
+
+        var gameBoard = $(".gameBoard").empty(); //czyszczenie planszy
+
+        //tablica z numerami kafelków
+        for (var _i = 0; _i < tilesCount; _i++) {
+            tiles.push(Math.floor(_i / 2));
+        }
+
+        //mieszanie tablicy z numerami kafelków - numery będą określały rodzaj kafelka
+
+        for (i = tilesCount - 1; i > 0; i--) {
+            var swap = Math.floor(Math.random() * i);
+            var tmp = tiles[i];
+            tiles[i] = tiles[swap];
+            tiles[swap] = tmp;
+        }
+
+        //stworzenie kafelków i dołączenie ich do planszy
+
+        for (i = 0; i < tilesCount; i++) {
+
+            var cell = $('<div class="cell"></div>'); //tworzę pustą komórkę
+            var tile = $('<div class="tile"<span class="front"></span><span class="reverse"></span>'); //tworzę kafelek
+
+            tile.addClass('cardType' + tiles[i]); //dodaję klasę cardType i losowy numer
+            tile.data('cardType', tiles[i]); //ustawiam atrybut data, dataset "cardType" na wartość losową
+            tile.data('index', i); //ustawiam data-index="i"
+
+            cell.append(tile); //wstawiam tile na końcu dzieci cell
+            gameBoard.append(cell); //wstawiam cell na końcu dzieci gameBoard
+        }
+
+        gameBoard.find('.ceil .tile').on("click", function () {
+            tileClicked($(this)); //kafelek został kliknięty
+        });
+    };
+
+    startGame();
+
+    tileClicked = function tileClicked(element) {
+
+        if (canGet) {
+            //canGet = true
+
+            if (!clickedTiles[0] || clickedTiles[0].data('index') != element.data('index')) {
+                //jeżeli jeszcze nie pobraliśmy 1 elementu lub jego indeksu nie ma w pobranych
+                clickedTiles.push(element); //index klikniętego kafelka dodajemy do tablicy
+                element.addClass("show"); //kafelkowi dajemy klasę "show
+            }
+
+            if (clickedTiles.length == 2) {
+                //clickedTiles zawiera numery aktualnie klkiniętych kafelków
+                canGet = false; //dwa kafelki kliknięte - nie można kliknąć następnych
+
+                if (clickedTiles[0].data('cardType') == clickedTiles[1].data('cardType')) {
+                    //sprawdzamy czy typ obu kafelków jest taki sam
+                    setTimeout(function () {
+                        deleteTiles(); //jeśli typ kafelków jest taki sam to usuwamy je
+                    }, 700);
+                } else {
+                    setTimeout(function () {
+                        resetTiles(); //jeśli typ kafelków jest różny to ukrywamy je
+                    }, 700);
+                }
+
+                movesCount++; //zwiększenie licznika
+                showMoves(movesCount); //pokazujemy wynik licznika w showMoves
+            }
+        }
+    };
+
+    deleteTiles = function deleteTiles() {
+        //deleteTiles jeśli typ kafelków jest taki sam
+        clickedTiles[0].fadeOut(function () {
+            $(this).remove();
+        });
+
+        clickedTiles[1].fadeOut(function () {
+            $(this).remove();
+
+            tilesPair++; //zwiększenie licznika odgadniętych par
+            if (tilesPair >= tilesCount / 2) {
+                gameOver(); //jeśli licznik par osiągnął liczbę kafelków/2 to koniec gry
+            }
+
+            clickedTiles = new Array();
+            canGet = true;
+        });
+
+        resetTiles = function resetTiles() {
+            clickedTiles[0].removeClass('show'); //ukrywanie kafelków przez usunięcie klasy show
+            clickedTiles[1].removeClass('show');
+            clickedTiles = new Array(); //wyczyszczenie tablicy z aktualnie kliknietymi kafelkami
+            canGet = true; //wyłączenie możliwości klikania
+        };
+    };
 });
-
-function revealCard(nr) {
-    alert(nr);
-}
 
 /***/ })
 /******/ ]);
